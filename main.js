b941d1d58fd942df9e2afb726fc6d7c1889512fc
Cobb <130980312+Cobb9527@users.noreply.github.com>
2023-09-09 23:51:32 +0800

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ let myImage = document.querySelector("img");
 
 myImage.onclick = function () {
   let mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/Firefox_logo.png") {
-    myImage.setAttribute("src", "images/Firefox2.png");
+  if (mySrc === "Firefox_logo.png") {
+    myImage.setAttribute("src", "Firefox2.png");
   } else {
-    myImage.setAttribute("src", "images/Firefox_logo.png");
+    myImage.setAttribute("src", "Firefox_logo.png");
   }
 };
 
